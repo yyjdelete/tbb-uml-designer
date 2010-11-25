d58971b007eb1f21e2dfb3d750c0190debca7e3e
yyjdelete@gmail.com <yyjdelete@gmail.com@ea16d80f-9c09-a047-287c-0bbe68886a0c>
2010-11-25 06:06:14 +0000

    尝试读取VC#2010文件（未完成）

--- trunk/src/UMLDes.Controls/Properties/AssemblyInfo.cs
+++ trunk/src/UMLDes.Controls/Properties/AssemblyInfo.cs
@@ -1,18 +1,17 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
-//using System.Runtime.InteropServices;
 
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("TBB UML Designer")]
-[assembly: AssemblyDescription("TBB UML Designer")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("UESTC TBB 项目组")]
-[assembly: AssemblyProduct("TBB UML Designer")]
-[assembly: AssemblyCopyright("CopyLeft")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle ("TBB UML Designer")]
+[assembly: AssemblyDescription ("TBB UML Designer")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("UESTC TBB 项目组")]
+[assembly: AssemblyProduct ("TBB UML Designer")]
+[assembly: AssemblyCopyright ("CopyLeft")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
 
 // 将 ComVisible 设置为 false 使此程序集中的类型
 // 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型，
@@ -32,7 +31,5 @@ using System.Runtime.CompilerServices;
 // 可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
-
+[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyFileVersion ("1.0.*")]
