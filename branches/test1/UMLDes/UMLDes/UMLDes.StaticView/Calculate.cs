f045a349e438fb8d65e443e0676ae24644abdb3e
yyjdelete@gmail.com <yyjdelete@gmail.com@ea16d80f-9c09-a047-287c-0bbe68886a0c>
2010-12-14 05:45:40 +0000

    清理耦合度计算代码，提高通用性 修正连线中部分右键菜单无效的错误

--- branches/test1/UMLDes/UMLDes/UMLDes.StaticView/Calculate.cs
+++ branches/test1/UMLDes/UMLDes/UMLDes.StaticView/Calculate.cs
@@ -9,44 +9,21 @@ namespace UMLDes.GUI {
 			double result;
 			int RelationValue;
 
-			// TODO 
 			PackageNum=GetPackageNum (curr);
-			
-
-			
 			RelationValue=GetRelationValue (curr,out LineNum,PackageNum);
 			result=(double) RelationValue/(double) PackageNum/(double) PackageNum;
-
 
 			return result;
 		}
 
 		private static int GetPackageNum (StaticView curr) {
 			int i=0;
-//					private static void SelectNameFor (GuiItem v,StaticView parent) {
-//			String name="",tname,ename="",type=(v.GetType ().Name);
-//			select (v,ref name,type,false);
-//			if (name.IndexOf ('1')!=-1)
-//				name=name.Substring (0,name.IndexOf ('1'));
-//			int i=1;
-//			do {
-//				tname=name+i.ToString ();
+
 			foreach (GuiObject d in curr.active_objects)
 				if(d is GuiPackage)
 					i++;
+
 			return i;
-//						if (d.GetType ().Name==type) {
-//							select (d as GuiItem,ref ename,type,false);
-//							if (ename.Equals (tname)) {
-//								tname=null;
-//								break;
-//							}
-//						}
-//					i++;
-//			}
-//			while (tname==null);
-//			select (v,ref tname,type,true);
-//		}
 		}
 
 		private static int GetRelationValue (StaticView curr,out int LineNum,int PackageNum) {
@@ -60,41 +37,22 @@ namespace UMLDes.GUI {
 			
 			for(PackageID1=0;PackageID1<PackageNum;PackageID1++)
 				RelationSign[PackageID1,PackageID1]=1;
-			//					private static void SelectNameFor (GuiItem v,StaticView parent) {
-			//			String name="",tname,ename="",type=(v.GetType ().Name);
-			//			select (v,ref name,type,false);
-			//			if (name.IndexOf ('1')!=-1)
-			//				name=name.Substring (0,name.IndexOf ('1'));
-			//			int i=1;
-			//			do {
-			//				tname=name+i.ToString ();
 			foreach (GuiObject d in curr.active_objects)
 				if (d is GuiConnection) {
 					LineNum++;
 					GCP1=(d as GuiConnection).children[0] as GuiConnectionPoint;
 					GCP2=(d as GuiConnection).children[1] as GuiConnectionPoint;
 					if ((GCP1.item is GuiPackage)&&(GCP2.item is GuiPackage)) {
-						PackageID1=int.Parse(GCP1.item_id.Substring (8))-1;
-						PackageID2=int.Parse(GCP2.item_id.Substring (8))-1;
+						PackageID1=int.Parse (GCP1.item_id.Split ('#')[1])-1;//GCP1.item_id.Substring (GCP1.item_id.IndexOf('#')+1)
+						PackageID2=int.Parse (GCP2.item_id.Split ('#')[1])-1;//GCP2.item_id.Substring (GCP2.item_id.IndexOf('#')+1)
 						RelationSign[PackageID1,PackageID2]=1;
 					}
 				}
 			for (PackageID1=0;PackageID1<PackageNum;PackageID1++)
 				for (PackageID2=0;PackageID2<PackageNum;PackageID2++)
 					RelationValue=RelationValue+RelationSign[PackageID1,PackageID2];
+
 			return RelationValue;
-			//						if (d.GetType ().Name==type) {
-			//							select (d as GuiItem,ref ename,type,false);
-			//							if (ename.Equals (tname)) {
-			//								tname=null;
-			//								break;
-			//							}
-			//						}
-			//					i++;
-			//			}
-			//			while (tname==null);
-			//			select (v,ref tname,type,true);
-			//		}
 		}
 	}
 }
